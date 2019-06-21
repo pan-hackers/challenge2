beeee55261f4ec2de8d73947468f589ae8299d63
gi4nks <gi4nks@gmail.com>
2019-06-21 22:41:19 +0200

    Working on Consumable Units

--- express-2/src/models/index.js
+++ express-2/src/models/index.js
@@ -4,6 +4,7 @@ import TokenKey from './key';
 import User from './user';
 import Company from './company';
 import Location from './location';
+import ConsumableUnit from './consumableUnit';
 import Message from './message';
 import Coin from './coin';
 import Block from './block';
@@ -11,7 +12,7 @@ import Blockchain from './blockchain';
 
 const connectDb = () => mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-const models = { User, Company, Location, Message, Coin, Block, Blockchain };
+const models = { User, Company, Location, ConsumableUnit, Message, Coin, Block, Blockchain };
 
 export { connectDb, TokenKey };
 
