10f4b15de2d7213d73b8a958b1e799c74d448e3e
mpro7 <justinstarlight@gmail.com>
2019-06-22 19:50:39 +0200

    hotfix

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -11,7 +11,7 @@ import { ShipmentService } from '../shipment.service';
 export class MainComponent implements OnInit {
   public viewState = 'main';
   public shipmentsArray = [];
-  public shipNum="LHR 12312312"
+  public shipNum = 'LHR 12312312'
   constructor(
     private readonly shipmentService: ShipmentService
   ) { }
@@ -33,7 +33,6 @@ export class MainComponent implements OnInit {
   public getShipments() {
     this.shipmentService.getAllShipments().subscribe(
       (res) => {
-<<<<<<< HEAD
         if (this.shipmentsArray === undefined || this.shipmentsArray.length === 0) {
           this.shipmentsArray = data;
         } else {
@@ -41,16 +40,6 @@ export class MainComponent implements OnInit {
         }
       }
     );
-
-=======
-        this.shipmentsArray = res;
-        if (this.shipmentsArray === undefined || this.shipmentsArray.length === 0) {
-          this.shipmentsArray = data;
-        }
-      }
-    );
-    
->>>>>>> bc99630227f82ff89b2a500ef52194e89f574a9f
   }
 
 }
