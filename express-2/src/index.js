5bc579b72ea25e5a0853f79e9de24479ab8c80c7
gi4nks <gi4nks@gmail.com>
2019-06-23 00:28:15 +0200

    added awb to shipment

--- express-2/src/index.js
+++ express-2/src/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(morgan('combined'));
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 /* Own middlewares */
 
