964019e13de5c756f03d76cc0f4c330d7adafe3f
Piotr Dziubecki <piotr.dziubecki@gmail.com>
2019-06-22 22:53:26 +0200

    config for aws

--- express-2/src/models/index.js
+++ express-2/src/models/index.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import fs from 'fs';
 
 import Company from './company';
 import Location from './location';
@@ -12,7 +13,16 @@ import Message from './message';
 import Block from './block';
 import Blockchain from './blockchain';
 
-const connectDb = () => mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+//const connectDb = () => mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+//Specify the Amazon DocumentDB cert
+var ca = [fs.readFileSync(__dirname + "/rds-combined-ca-bundle.pem")];
+var devDBURL = 'mongodb://kosmos:root1234@docdb-2019-06-21-18-57-30.cluster-cmclwftd5yib.us-east-1.docdb.amazonaws.com:27017/test?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred';
+
+const connectDb = () => mongoose.connect(devDBURL, {
+    sslValidate: true,
+    sslCA:ca,
+    useNewUrlParser: true
+});
 
 const models = { Company, Location, ConsumableUnit, TradeUnit, 
     LogisticUnit, Shipment, Message, 
