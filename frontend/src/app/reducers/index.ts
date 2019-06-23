5c2d8af347870faf55e11f987b01d4f8d128482d
mpro7 <justinstarlight@gmail.com>
2019-06-23 09:42:40 +0200

    reducers fix

--- frontend/src/app/reducers/index.ts
+++ frontend/src/app/reducers/index.ts
@@ -2,4 +2,4 @@ import { combineReducers } from '@ngrx/store';
 
 import { shipmentReducer as shipmentState } from './shipment-reducer';
 
-export const RootReducer = combineReducers(shipmentState);
+export const RootReducer = combineReducers({ shipmentState });
